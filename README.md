8d32449f0a9bc7b992d3a0bc1fb03033f7202a6a
Andreas Loizides <andreas16700@icloud.com>
2025-02-20 04:15:14 +0200

    now updates cloudflare records too

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# ⚡ Save current public IPv4 to Appwrite Cloud
+# ⚡ Save current public IPv4 to Appwrite Cloud and update Cloudflare DNS records 
 
 I self-host an appwrite instance on my home network. I don't have a static IP which means it changes sometimes.
 
 This function is meant to run on a regular basis on the self hosted instance.
+
+If the public IP changes, this function saves this on appwrite cloud, as well as updates the DNS records on cloudflare (the ones that point to the old public IPv4 address)
 
 This function fetches the current public IPv4 address from https://api.ipify.org and saves it on a database on Appwrite Cloud.
 
@@ -21,6 +23,12 @@ The above documents are updated iff the currently saved address on AW Cloud diff
 
 
 
-| Key               | Value                    |
-|-------------------|--------------------------|
+| Key          | Value                      |
+|--------------|----------------------------|
 | AW_CLOUD_KEY | `the_api_key_for_aw_cloud` |
+| CF_KEY       | `cloudflare_api_key`       |
+| CF_TOKEN     | `cloudflare_api_token`     |
+| CF_ZONE      | `cloudflare_zone_id`       |
+| CF_EMAIL     | `cloudflare_account_email` |
+
+*The cloudflare token must have edit permission for DNS records on the specified zone.*
--- src/main.py
+++ src/main.py
@@ -2,7 +2,19 @@ from appwrite.client import Client
 from appwrite.services.users import Users
 from appwrite.services.databases import Databases
 from appwrite.exception import AppwriteException
-import os, requests
+import os, requests, json
+from datetime import datetime
+from cloudflare import Cloudflare
+from cloudflare.types.dns.batch_patch_param import ARecord
+
+def custom_serializer(obj):
+    if isinstance(obj, datetime):
+        # Convert datetime to an ISO-formatted string
+        return obj.isoformat()
+    if hasattr(obj, '__dict__'):
+        # For other objects, use their __dict__ attribute
+        return obj.__dict__
+    raise TypeError(f"Type {type(obj)} not serializable")
 
 client = (
         Client()
@@ -16,6 +28,35 @@ url = "https://api.ipify.org"
 DB_ID = "constants"
 COLL_ID = "ip"
 
+
+AW_CLOUD_KEY=os.environ["AW_CLOUD_KEY"]
+CF_TOKEN = os.environ["CF_TOKEN"]
+CF_KEY = os.environ["CF_KEY"]
+CF_EMAIL = os.environ["CF_EMAIL"]
+CF_ZONE = os.environ["CF_ZONE"]
+
+client = Cloudflare(
+    api_email=CF_EMAIL,
+    api_key= CF_KEY,
+    api_token=CF_TOKEN,
+)
+def update_dns_records(oldip: str, newip: str, context):
+    # get all records
+    page = client.dns.records.list(zone_id=CF_ZONE)
+    # filter the ones pointing to the old ip
+    records_of_interest = [r for r in page.result if r.content == oldip]
+    if len(records_of_interest) <= 0:
+        context.log(f"No records on cloudflare that point to the old ip: {oldip}!")
+    else:
+        context.log(f"will update {len(records_of_interest)} records on cloudflare that point to {oldip}!:")
+        t = ", ".join([r.name for r in records_of_interest])
+        context.log(t)
+    # update those
+    try:
+        response = client.dns.records.batch(zone_id=CF_ZONE, patches=[ARecord(id=re.id, content=newip) for re in records_of_interest])
+        context.log(json.dumps(response, default=custom_serializer, indent=5))
+    except Exception as e:
+        context.error(f"error updating dns records: {e}")
 
 
 def fetch_public_ipv4(context):
@@ -53,13 +94,18 @@ def main(context):
 
         current_saved = get_ip(doc_id="current")
         if current_saved == current_ip:
-            context.log(f"current ip is the one that's saved: {current_ip}")
+            msg = f"current ip is the one that's saved: {current_ip}"
+            context.log(msg)
+            return context.res.text(msg)
         context.log(f"saving new ip {current_ip} and old ip {current_saved}")
         save_ip(ip=current_saved, doc_id="old")
 
         save_ip(ip=current_ip, doc_id="current")
 
         context.log(f"done!")
+
+        context.log(f"updating cloudflare records...")
+        update_dns_records(oldip=current_saved, newip=current_ip, context=context)
 
 
         # Log messages and errors to the Appwrite Console
